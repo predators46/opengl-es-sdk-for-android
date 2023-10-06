02ee16cd5fd8b6bd37bba20b28bf50706f9c2463
Predators <31931530+predators46@users.noreply.github.com>
2023-10-06 22:09:56 +0800

    Update cc.sql

--- cc.sql
+++ cc.sql
@@ -2,13 +2,14 @@
   acctsessionid varchar
   acctuniqueid varchar
   username varchar
-  groupname varchar
   realm varchar
   nasipaddress varchar
   nasportid varchar
   nasporttype varchar
-  acctstarttime datetime 
-  acctstoptime datetime 
+  acctstarttime datetime
+  acctupdatetime datetime
+  acctstoptime datetime
+  acctinterval int
   acctsessiontime int
   acctauthentic varchar
   connectinfo_start varchar
@@ -21,15 +22,21 @@
   servicetype varchar
   framedprotocol varchar
   framedipaddress varchar
-  acctstartdelay int
-  acctstopdelay int
-  xascendsessionsvrkey varchar
-  PRIMARY KEY  (radacctid),
+  framedipv6address varchar
+  framedipv6prefix varchar
+  framedinterfaceid varchar
+  delegatedipv6prefix varchar
+  PRIMARY KEY (radacctid),
+  UNIQUE KEY acctuniqueid (acctuniqueid),
   KEY username (username),
   KEY framedipaddress (framedipaddress),
+  KEY framedipv6address (framedipv6address),
+  KEY framedipv6prefix (framedipv6prefix),
+  KEY framedinterfaceid (framedinterfaceid),
+  KEY delegatedipv6prefix (delegatedipv6prefix),
   KEY acctsessionid (acctsessionid),
   KEY acctsessiontime (acctsessiontime),
-  KEY acctuniqueid (acctuniqueid),
   KEY acctstarttime (acctstarttime),
+  KEY acctinterval (acctinterval),
   KEY acctstoptime (acctstoptime),
   KEY nasipaddress (nasipaddress)
